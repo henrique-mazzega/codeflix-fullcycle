333c2b00800c319d96f2bfbd900fbbe8c4557ece
henrique.mazzega <henrique_lm08@hotmail.com>
2024-10-09 12:23:29 -0300

    projeto codeflix

---
+++ aplication/src/main/java/com/fullcycle/admin/catalogo/aplication/UseCase.java
@@ -0,0 +1,10 @@
+package com.fullcycle.admin.catalogo.aplication;
+
+import com.fullcycle.admin.catalogo.domain.Category;
+
+public class UseCase {
+
+    public Category execute() {
+        return new Category();
+    }
+}
--- src/main/java/com/fullcycle/admin/catalogo/Main.java
+++
@@ -1,7 +0,0 @@
-package com.fullcycle.admin.catalogo;
-
-public class Main {
-    public static void main(String[] args) {
-        //teste git
-    }
-}
